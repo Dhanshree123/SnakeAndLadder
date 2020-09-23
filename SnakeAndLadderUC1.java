e70466918c2503621c5779039237ea1bf181732f
Dhanshree123 <2016181@iiitdmj.ac.in>
2020-09-23 09:32:51 +0530

    Add UC1

---
+++ SnakeAndLadderUC1.java
@@ -0,0 +1,12 @@
+package com.BridgeLabz.SnakeAndLadder;
+
+public class SnakeAndLadderUC1 {
+
+	public static void main(String[] args) {
+		int NO_OF_PLAYERS = 1;
+		int START_POSITION = 0;
+		
+		System.out.println("Number of Players: "+NO_OF_PLAYERS);
+		System.out.println("Position of Player: "+START_POSITION);
+	}
+}
