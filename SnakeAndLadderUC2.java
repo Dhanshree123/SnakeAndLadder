880e43405752a61ba42e894d27443cb1fef3b016
Dhanshree123 <2016181@iiitdmj.ac.in>
2020-09-23 09:35:22 +0530

    Add UC2

---
+++ SnakeAndLadderUC2.java
@@ -0,0 +1,10 @@
+package com.BridgeLabz.SnakeAndLadder;
+
+public class SnakeAndLadderUC2 {
+ public static void main(String[] args) {
+	    int NO_OF_PLAYERS = 1;
+		double DICE_OUTPUT = Math.floor(Math.random() *10) %6 + 1.0;
+		System.out.println("Dice Output: "+(int)DICE_OUTPUT);
+		
+}
+}
